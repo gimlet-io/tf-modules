d8731458b6cf91d68dda1a39aae8ec2c6dda1079
Laszlo Fogas <laszlo@gimlet.io>
2023-06-05 13:14:55 +0200

    Missing file

---
+++ azure/postgresql-flexible-server-database/variables.tf
@@ -0,0 +1,39 @@
+variable "server_id" {
+  type = string
+}
+
+variable "database" {
+  type = string
+}
+
+variable "user" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+}
+
+variable "host" {
+  type = string
+}
+
+variable "username" {
+  type = string
+}
+
+variable "password" {
+  type = string
+}
